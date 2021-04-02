a8edaa55bacfeeeab301b657929ebc29a7e805db
rikima0519 <s.rikima@icloud.com>
2021-04-02 13:50:23 +0900

    BMIのプログラム実装

--- 02/bmi/practice/js/app.js
+++ 02/bmi/practice/js/app.js
@@ -2,4 +2,11 @@ let weight;
 weight = prompt('BMIを測定します。まずはあなたの体重（kg）を入力してください');
 
 let height;
-height = prompt('次に身長を入力してください')
+height = prompt('BMIを測定します。次に身長を入力してください')
+
+let bmi = weight / (height * height)
+
+let message = 'あなたのBMIは「' + bmi + '」です。';
+
+alert(message);
+
