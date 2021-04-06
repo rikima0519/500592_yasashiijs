b575a3109e3a2f2c0d683d2c718b20d3943617f6
rikima0519 <s.rikima@icloud.com>
2021-04-06 15:45:09 +0900

    おみくじ実装

---
+++ 08/array/practice/js/app.js
@@ -0,0 +1,13 @@
+//おみくじの結果データを作成
+results = ['大吉', '吉', '中吉', '小吉', '凶'];
+
+
+console.log(results);
+
+
+console.log(results[0]);
+
+
+for (let i = 0; i < results.length; i++) {
+  console.log('index:' + i + 'データ:' + results[i]);
+}
