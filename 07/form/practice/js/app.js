c85be6b3904ea9f5f0bc33dd909d6940db94de97
rikima0519 <s.rikima@icloud.com>
2021-04-06 13:32:59 +0900

    タイマー機能実装

--- 07/form/practice/js/app.js
+++ 07/form/practice/js/app.js
@@ -7,17 +7,26 @@ let textarea = document.getElementById('textarea');
 // 文字数制限
 let maxTextNum = textarea.getAttribute('maxlength');
 
+let remainingTimeNum = 10;
+
 let textMessage = document.createElement('div');
 let parent = textarea.parentElement;
 parent.insertBefore(textMessage, textarea);
 
+let timeMessage = document.createElement('div');
+parent.insertBefore(timeMessage, null);
+
 button.addEventListener('click', function () {
   form.style.display = 'block';
-})
-
-
-
-
+  let timeId = setInterval(function () {
+    timeMessage.innerHTML = '<p>制限時間:' + remainingTimeNum + '秒</p>';
+    if (remainingTimeNum <= 0) {
+      alert('制限時間終了');
+      clearInterval(timeId);
+    }
+    remainingTimeNum--;
+  }, 1000);
+});
 
 
 // テキストエリアでキーをタイプしたとき
